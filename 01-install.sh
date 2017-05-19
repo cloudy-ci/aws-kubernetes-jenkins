b0bb9dc42b1c773c6679c62b0573d73e266328fc
Johannes Ebke <johannes.ebke@tngtech.com>
2017-05-19 12:17:27 +0200

    Mac Compatibility

--- 01-install.sh
+++ 01-install.sh
@@ -1,10 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -eu
+
+if [ "$(uname)" == "Darwin" ]; then
+	ARCH=darwin
+else
+    ARCH=linux
+fi
 
 export PATH=$PWD:$PATH
 
 KUBERNETES_RELEASE=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
-KOPS_RELEASE=$(curl -s https://github.com/kubernetes/kops/releases/ | grep -o '/kubernetes/kops/releases/download/.*/kops-linux-amd64' | sort -V | tail -n1 | cut -d/ -f6)
+KOPS_RELEASE=$(curl -s https://github.com/kubernetes/kops/releases/ | grep -o "/kubernetes/kops/releases/download/.*/kops-$ARCH-amd64" | sort -V | tail -n1 | cut -d/ -f6)
 
 echo "kops $KOPS_RELEASE and kubernetes $KUBERNETES_RELEASE found."
 
@@ -12,7 +18,7 @@ if kubectl help > /dev/null; then
     echo "kubectl available in PATH, skipping..."
 else
     echo "downloading kubectl $KUBERNETES_RELEASE"
-    wget https://storage.googleapis.com/kubernetes-release/release/$KUBERNETES_RELEASE/bin/linux/amd64/kubectl
+    wget https://storage.googleapis.com/kubernetes-release/release/$KUBERNETES_RELEASE/bin/$ARCH/amd64/kubectl
     chmod +x kubectl
 fi
 
@@ -20,8 +26,8 @@ if kops help > /dev/null; then
     echo "kops available in PATH, skipping..."
 else
     echo "downloading kops $KOPS_RELEASE"
-    wget https://github.com/kubernetes/kops/releases/download/$KOPS_RELEASE/kops-linux-amd64
-    mv kops-linux-amd64 kops
+    wget https://github.com/kubernetes/kops/releases/download/$KOPS_RELEASE/kops-$ARCH-amd64
+    mv kops-$ARCH-amd64 kops
     chmod +x kops
 fi
 
--- 05-kubernetes-start-dashboard.sh
+++ 05-kubernetes-start-dashboard.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 kubectl create -f https://raw.githubusercontent.com/kubernetes/kops/master/addons/kubernetes-dashboard/v1.6.0.yaml
